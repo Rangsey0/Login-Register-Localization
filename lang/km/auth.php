72278b98f36092e3caf0c37e449ca9d4f486a9ed
Rangsey <sarangseykol@gmail.com>
2024-05-03 23:11:01 +0700

    first commit

---
+++ lang/km/auth.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'សមត្ថភាពទាំងនេះមិនផ្គូផ្គងនឹងកំណត់ត្រារបស់យើងទេ។',
+    'password' => 'ពាក្យសម្ងាត់ដែលបានផ្ដល់មិនត្រឹមត្រូវ។',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'laravel' => 'ឡារាវេល',
+    'email' => 'អុីមេល',
+    'password' => 'ពាក្យសម្ងាត់',
+    'rememberme' => 'ចងចាំខ្ញុំ',
+    'forgotyourpassword' => 'ភ្លេចពាក្យសម្ងាត់របស់អ្នក?',
+    'login' => 'ចូល',
+    'register' => 'ចុះឈ្មោះ',
+    'name' => 'ឈ្មោះ',
+    'confirmpassword' => 'បញ្ជាក់ពាក្យសម្ងាត់',
+    'alreadyregistered' => 'ចុះឈ្មោះហើយ?',
+    'forgotyourpasswordtxt' => 'ភ្លេចពាក្យសម្ងាត់របស់អ្នក? គ្មាន​បញ្ហា។ គ្រាន់តែអនុញ្ញាតឱ្យយើងដឹងពីអាសយដ្ឋានអ៊ីមែលរបស់អ្នក ហើយយើងនឹងផ្ញើអ៊ីមែលទៅអ្នកនូវតំណកំណត់ពាក្យសម្ងាត់ឡើងវិញដែលនឹងអនុញ្ញាតឱ្យអ្នកជ្រើសរើសពាក្យសម្ងាត់ថ្មី។',
+    'emailreset' => 'តំណកំណត់ពាក្យសម្ងាត់ឡើងវិញតាមអ៊ីមែល'
+
+];
